8611b3ffac450dee52b0dd3a3411c35f56177df2
Push9828 <pushkar.thakur28@gmail.com>
2020-07-14 15:39:21 +0530

    🎉 Initial Commit

---
+++ src/components/CharacterGrid.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CharacterGrid = () => {
+  return <section className="cards"></section>;
+};
+
+export default CharacterGrid;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="topHeader">The Rick and Morty API</header>;
+};
+
+export default Header;
