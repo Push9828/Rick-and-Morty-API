21642985e4bdd0073fbae30d82595ee3f0c1a8b4
Push9828 <pushkar.thakur28@gmail.com>
2020-07-16 14:10:22 +0530

    :package: Final Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/Header";
+import CharacterGrid from "./components/CharacterGrid";
+import "./App.css";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await axios(`https://rickandmortyapi.com/api/character`);
+
+      console.log(result.data.results);
+
+      setCharacters(result.data.results);
+      setIsLoading(false);
+    };
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="containter">
       <Header />
+      <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
 };
--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Spinner from "./Spinner";
+import CharacterItem from "./CharacterItem";
 
-const CharacterGrid = () => {
-  return <section className="cards"></section>;
+const CharacterGrid = ({ characters, isLoading }) => {
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="flexContainer">
+      {characters.map((character) => (
+        <CharacterItem key={character.id} character={character} />
+      ))}
+    </section>
+  );
 };
 
 export default CharacterGrid;
